85b2736814eccf9e947153bb3cb6badc30d28d87
iZa106 <rizaarvine2@gmail.com>
2026-09-10 17:05:55 +0700

    Arsitektur Laravel, Struktur Folder, dan Routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
+
+    
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+});
+
+Route::get('/about', function () {
+    return 'Ini page about POS Barokah Mart';
 });
